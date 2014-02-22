7bb8de4a7e6aaf6759976d566854c152080b9342
ericpoe <eric.poe@gmail.com>
2014-02-21 20:05:31 -0600

    Renamed dart_codesheet, which was to be a single document for all of my notes, to something more specific. Each topic will be its own file for now.

---
+++ comments.dart
@@ -0,0 +1,25 @@
+/** Comments */
+
+/**
+ * This is a Dart Docblock.
+ *
+ * This is what one would use to document a function, method, or class.
+ *
+ * ##Markdown##
+ * This is **huge**, one can use
+ * [markdown](https://www.dartlang.org/articles/doc-comment-guidelines/#markup)
+ * to help format the docblock.
+ */
+
+/// This is a different kind of Dart Docblock.
+///
+/// I could not find any documentation about it beyond a quick mention of it in
+/// https://www.dartlang.org/docs/spec/latest/dart-language-specification.html#h.gey5ehc2i2ad
+
+/* This is a
+multi-line
+comment */
+
+// This is a single-line comment
+// Each comment begins wherever the line has a "//" and ends at the end of the
+// line.
